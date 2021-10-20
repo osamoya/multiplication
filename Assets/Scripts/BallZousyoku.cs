c3e90eaf6e2b4e0204027ed70a4ab2eab528a740
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-10-21 02:44:15 +0900

    [Add]実験するスクリプトを仮置き
    とりあえずガワだけ作成ということで

---
+++ Assets/Scripts/BallZousyoku.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallZousyoku : MonoBehaviour
+{/// <summary>
+/// ボールを増殖させるスクリプト
+/// コピった奴はリストに追加する
+/// 2倍増だけ考える
+/// このスクリプトを複製みたいな感じでやる
+/// 親を考えると結局どこかで重い処理が挟まる気がするので考えたくない
+/// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Zousyoku()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerList_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerList_Script : MonoBehaviour
+{/// <summary>
+/// ここにはリストが存在する
+/// 基本的にすべてのボールはそこに入る
+/// ただし、入れるものはGameObjectではない
+/// 自作のクラス(というかスクリプト)を入れる
+/// で、ここでは
+/// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
