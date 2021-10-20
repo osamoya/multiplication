b5df3c1f128ad18569b17feaaf532895dfa467d0
TsubasaKatsumata_DESKTOP-GGHJ72S <osamumoyamoyasii17@gmail.com>
2021-10-21 03:14:00 +0900

    [update]リスト側のやることを記述
    回して呼び出しのみ
    Startで最初の要素を入れているのは気を付けないといけないかもしれない

--- Assets/Scripts/PlayerList_Script.cs
+++ Assets/Scripts/PlayerList_Script.cs
@@ -10,15 +10,28 @@ public class PlayerList_Script : MonoBehaviour
 /// 自作のクラス(というかスクリプト)を入れる
 /// で、ここでは
 /// </summary>
+
+    List<BallZousyoku> list;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject firstball = GameObject.FindGameObjectWithTag("player");
+        list.Add(firstball.GetComponent<BallZousyoku>());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DOZousyokuAll()
+    {
+        int prenum = list.Count;//要素の数なので注意！
+        for (int i=0;i<prenum;i++)
+        {
+            list[i].Zousyoku();
+        }
     }
 }
